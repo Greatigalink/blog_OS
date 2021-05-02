a04d39ab192cf02892cf8a1c375fd29cc9801df3
Greatigalink <1652891875@qq.com>
2021-05-02 13:50:31 +0800

    feat: 完善你的余生文件

--- pages/function/your-life/index.js
+++ pages/function/your-life/index.js
@@ -1,5 +1,21 @@
+import { Nav, BackTop, Footer } from '../../../component/export'
+import { useSelector } from "react-redux"
+
+const getStore = function() {
+  return useSelector(
+    (state) => ({
+      clock: state.basicState.clock
+    })
+  )
+}
+
 export default function YourLife() {
+  const { clock } = getStore();
   return(
-    <div></div>
+    <div>
+      <Footer clock={clock} />
+      <Nav nowNav={'/function/your-life'} />
+      <BackTop />
+    </div>
   )
 }
