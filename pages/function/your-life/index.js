e04e46d754bb19bd1aba1f479d9c848dd949a377
Greatigalink <1652891875@qq.com>
2021-05-02 15:22:53 +0800

    feat: 增加样式

--- pages/function/your-life/index.js
+++ pages/function/your-life/index.js
@@ -1,5 +1,6 @@
 import { Nav, BackTop, Footer } from '../../../component/export'
 import { useSelector } from "react-redux"
+import style from './yourlife.module.scss'
 
 const getStore = function() {
   return useSelector(
@@ -12,7 +13,7 @@ const getStore = function() {
 export default function YourLife() {
   const { clock } = getStore();
   return(
-    <div>
+    <div style={style.your_life}>
       <Footer clock={clock} />
       <Nav nowNav={'/function/your-life'} />
       <BackTop />
